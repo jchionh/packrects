9440029d84952da0f43897de42f3135c9a717d54
Jason Chionh <jasonchionh@gmail.com>
2013-06-02 02:11:59 -0700

    implemented overlapping of basic rects

--- src/com/appspot/packrects/engine/RectUtils.java
+++ src/com/appspot/packrects/engine/RectUtils.java
@@ -21,4 +21,36 @@ public class RectUtils {
         "#800080"
     };
     
+    /**
+     * given 2 basic rects, do they overlap?
+     * @param a
+     * @param b
+     * @return
+     */
+    public static boolean doRectsOverlap(BasicRect a, BasicRect b) {
+        int aMinX = a.x;
+        int aMinY = a.y;
+        int aMaxX = a.x + a.w;
+        int aMaxY = a.y + a.h;
+
+        int bMinX = b.x;
+        int bMinY = b.y;
+        int bMaxX = b.x + b.w;
+        int bMaxY = b.y + b.h;
+
+        if (aMaxX <= bMinX) {
+            return false; // a is left of b
+        }
+        if (aMinX >= bMaxX) {
+            return false; // a is right of b
+        }
+        if (aMaxY <= bMinY) {
+            return false; // a is above b
+        }
+        if (aMinY >= bMaxY) {
+            return false; // a is below b
+        }
+        return true; // boxes overlap
+    }
+    
 }
